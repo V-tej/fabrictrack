dd244aa9dbf0e7e52cec3e87e61f3f20349d08f8
V-tej <varuntej7009@gmail.com>
2026-06-21 19:30:16 +0530

    Add auto user creation for Render deployment

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create the 6 app users (skips if already exist)
+python manage.py create_initial_users
+
+# Auto-create superuser from environment variables (skips if already exists)
+python manage.py createsuperuser --no-input || true
---
+++ core/management/commands/create_initial_users.py
@@ -0,0 +1,85 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from core.models import UserProfile
+
+
+class Command(BaseCommand):
+    help = 'Create the 6 initial FabricTrack users'
+
+    USERS = [
+        {
+            'username': 'cuttingmaster',
+            'password': 'cutting@123',
+            'first_name': 'Cutting',
+            'last_name': 'Master',
+            'person_type': 'P1',
+        },
+        {
+            'username': 'lakshay',
+            'password': 'lakshay@123',
+            'first_name': 'Lakshay',
+            'last_name': '',
+            'person_type': 'P2',
+        },
+        {
+            'username': 'rahul',
+            'password': 'rahul@123',
+            'first_name': 'Rahul',
+            'last_name': '',
+            'person_type': 'P3',
+        },
+        {
+            'username': 'miyaji',
+            'password': 'miyaji@123',
+            'first_name': 'Miya',
+            'last_name': 'Ji',
+            'person_type': 'P4',
+        },
+        {
+            'username': 'jobworker',
+            'password': 'jobworker@123',
+            'first_name': 'Job',
+            'last_name': 'Worker',
+            'person_type': 'P5',
+        },
+        {
+            'username': 'finishing',
+            'password': 'finishing@123',
+            'first_name': 'Finishing',
+            'last_name': '',
+            'person_type': 'P6',
+        },
+    ]
+
+    def handle(self, *args, **options):
+        self.stdout.write('Creating FabricTrack users...')
+        created_count = 0
+        skipped_count = 0
+
+        for user_data in self.USERS:
+            username = user_data['username']
+
+            if User.objects.filter(username=username).exists():
+                self.stdout.write(f'  [SKIP] {username} already exists')
+                skipped_count += 1
+                continue
+
+            user = User.objects.create_user(
+                username=username,
+                password=user_data['password'],
+                first_name=user_data['first_name'],
+                last_name=user_data['last_name'],
+                is_staff=False,
+            )
+
+            UserProfile.objects.update_or_create(
+                user=user,
+                defaults={'person_type': user_data['person_type']}
+            )
+
+            self.stdout.write(
+                f'  [OK] {username} | {user_data["person_type"]} | password: {user_data["password"]}'
+            )
+            created_count += 1
+
+        self.stdout.write(f'Done: {created_count} created, {skipped_count} skipped.')
