deabbdfecd6544a91f608f17b8e9162f96be6145
V-tej <varuntej7009@gmail.com>
2026-09-03 22:20:31 +0530

    fix: add real DB operations to migration 0068 and update Kanban/Edit modal features

--- core/migrations/0068_chatmessage_voice.py
+++ core/migrations/0068_chatmessage_voice.py
@@ -8,33 +8,28 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.SeparateDatabaseAndState(
-            state_operations=[
-                migrations.AddField(
-                    model_name='chatmessage',
-                    name='audio_data',
-                    field=models.BinaryField(blank=True, null=True),
-                ),
-                migrations.AddField(
-                    model_name='chatmessage',
-                    name='audio_duration',
-                    field=models.PositiveIntegerField(default=0),
-                ),
-                migrations.AddField(
-                    model_name='chatmessage',
-                    name='audio_mime',
-                    field=models.CharField(blank=True, max_length=100),
-                ),
-                migrations.AlterField(
-                    model_name='chatmessage',
-                    name='message_type',
-                    field=models.CharField(
-                        choices=[('text', 'Text'), ('task', 'Task card'), ('voice', 'Voice note')],
-                        default='text',
-                        max_length=10,
-                    ),
-                ),
-            ],
-            database_operations=[],
+        migrations.AddField(
+            model_name='chatmessage',
+            name='audio_data',
+            field=models.BinaryField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='chatmessage',
+            name='audio_duration',
+            field=models.PositiveIntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='chatmessage',
+            name='audio_mime',
+            field=models.CharField(blank=True, max_length=100),
+        ),
+        migrations.AlterField(
+            model_name='chatmessage',
+            name='message_type',
+            field=models.CharField(
+                choices=[('text', 'Text'), ('task', 'Task card'), ('voice', 'Voice note')],
+                default='text',
+                max_length=10,
+            ),
         ),
     ]
